4bacc630a602ddc4a73e746c438b8874e8c6a513
mohammedsjnoube <76224485+mohammedsjnoube@users.noreply.github.com>
2020-12-24 15:18:42 +0200

    Update creds.py

--- creds.py
+++ creds.py
@@ -2,6 +2,6 @@ import os
 
 
 class my():
-    BOT_TOKEN = os.environ["BOT_TOKEN"]
-    API_ID = int(os.environ["API_ID"])
-    API_HASH = os.environ["API_HASH"]
+    BOT_TOKEN = os.environ["1479038923:AAHDurCIMjoG3yLTeTtMAV6sePj5gn8ogdM"]
+    API_ID = int(os.environ["2938219"])
+    API_HASH = os.environ["5172739520e2df35a286d98a928dc0cb"]
